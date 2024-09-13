27cb92e04409a6647f2988018540892641536e16
Alan Hauser <diefthyntis@gmail.com>
2024-09-13 21:31:42 +0200

    New fibo opérationnelle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fibonacci';
+  alwaysGrowing!:Subscription;
+  numberProduction$!:Observable<number>;
+  item:number=0;
+  itemSeries!:string;
+  magicNumber!:number;
+  counter!:number;
+  magicSeries!:string;
+
+  ngOnInit(): void {
+    this.numberProduction$ = interval(1000);
+    this.itemSeries="";
+    this.magicSeries="";
+    this.alwaysGrowing = this.numberProduction$.subscribe(newone => {
+      this.counter =newone ;
+      let tmp =this.item ;
+      this.item = this.item+ newone;
+      this.itemSeries = this.itemSeries + " " + this.item;
+      this.magicNumber= this.item/tmp;
+      this.magicSeries = this.magicSeries + "  f(" + newone + ")=" + this.item + "/" + tmp + "=" + this.magicNumber + "";
+    });
+
+  }
 }
